878a25ec3760b6e76368c0b694bf7d0286b9ab43
Andre Hartiwan <blwfish705@gmail.com>
2013-12-22 13:22:20 +0700

    Finish Chapter 2

---
+++ Chapter_1/src/org/blenderhead/chapter1/ForTest.java
@@ -0,0 +1,18 @@
+package org.blenderhead.chapter1;
+
+/*
+	Demonstrate the for loop.
+*/
+
+public class ForTest {
+
+	public static void main(String[] args) {
+
+		int x;
+		  
+	    for(x = 0; x<10; x = x+1)
+	      System.out.println("This is x: " + x);
+
+	}
+
+}
