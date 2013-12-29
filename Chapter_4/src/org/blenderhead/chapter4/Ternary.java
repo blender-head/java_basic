a25f274f9bf5f904eb6f23cb71dc1253110a83a2
Andre Hartiwan <blwfish705@gmail.com>
2013-12-29 12:56:35 +0700

    Finish Chapter 4

---
+++ Chapter_4/src/org/blenderhead/chapter4/Ternary.java
@@ -0,0 +1,22 @@
+package org.blenderhead.chapter4;
+
+//Demonstrate ?.
+public class Ternary {
+
+	public static void main(String[] args) {
+
+		int i, k;
+
+	    i = 10;
+	    k = i < 0 ? -i : i; // get absolute value of i
+	    System.out.print("Absolute value of ");
+	    System.out.println(i + " is " + k);
+
+	    i = -10;
+	    k = i < 0 ? -i : i; // get absolute value of i
+	    System.out.print("Absolute value of ");
+	    System.out.println(i + " is " + k);
+
+	}
+
+}
