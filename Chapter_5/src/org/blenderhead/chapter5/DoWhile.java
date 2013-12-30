f87f4f45cb245b5fbb1603fc138d268e3fbaa35b
Andre Hartiwan <blwfish705@gmail.com>
2013-12-30 10:20:07 +0700

    Finish Chapter 5

---
+++ Chapter_5/src/org/blenderhead/chapter5/DoWhile.java
@@ -0,0 +1,17 @@
+package org.blenderhead.chapter5;
+
+//Demonstrate the do-while loop.
+public class DoWhile {
+
+	public static void main(String[] args) {
+
+		int n = 10;
+
+	    do {
+	      System.out.println("tick " + n);
+	      n--;
+	    } while(n > 0);
+
+	}
+
+}
