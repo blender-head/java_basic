c98e9d85285675e48aadda0ad2ecea88fbfe134b
Andre Hartiwan <blwfish705@gmail.com>
2013-12-31 09:25:48 +0700

    Finish Chapter 8

---
+++ Chapter_7/src/org/blenderhead/chapter7/ClassConst.java
@@ -0,0 +1,16 @@
+package org.blenderhead.chapter7;
+
+public class ClassConst {
+	
+	final int FILE_NEW = 1;
+	final int FILE_OPEN = 2;
+	final int FILE_SAVE = 3;
+	final int FILE_SAVEAS = 4;
+	final int FILE_QUIT = 5;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter_7/src/org/blenderhead/chapter7/CommandLine.java
@@ -0,0 +1,13 @@
+package org.blenderhead.chapter7;
+
+//Display all command line arguments.
+public class CommandLine {
+
+	public static void main(String[] args) {
+
+		for(int i=0; i<args.length; i++)
+		      System.out.println("args[" + i + "]: " + args[i]);
+
+	}
+
+}
