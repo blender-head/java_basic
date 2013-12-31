36bc66ae1d1ee9613c99809b670c92b741e547d9
Andre Hartiwan <blwfish705@gmail.com>
2013-12-31 22:04:33 +0700

    Finish Chapter 13

---
+++ Chapter_13/src/org/blenderhead/chapter13/WriteDemo.java
@@ -0,0 +1,16 @@
+package org.blenderhead.chapter13;
+
+//Demonstrate System.out.write().
+public class WriteDemo {
+
+	public static void main(String[] args) {
+
+		int b;
+
+	    b = 'A';
+	    System.out.write(b);
+	    System.out.write('\n');
+
+	}
+
+}
